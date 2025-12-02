3ac2151289dede69dea18709e58c5bbba71133c4
Katherine Phan <katherinephan21@gmail.com>
2025-12-01 18:03:14 -0800

    Add styling for cart icon in desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export const Navbar:React.FC = () => {
                     ))}
                 
                     {/* Desktop cart icon */}
-                    <Link to='/cart' className='text-text-dark'>
+                    <Link to='/cart' className='relative p-2 text-text-dark hiver:text-text-light focus:outline-none transition-colors' aria-label='Shopping cart'>
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
